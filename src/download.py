8bed36fbeaadf881a0f23a0f1bce01cf5be52869
sevenwhiteclouds <kemaldonado@csumb.edu>
2023-09-14 01:10:55 -0700

    making http requests

--- src/download.py
+++ src/download.py
@@ -1,7 +1,13 @@
-import requests
+import urllib.request as request
+import json
+
+EMBED_URL = "http://fast.wistia.net/embed/iframe/"
 
 if __name__ == "__main__":
   file = input("Enter file name: ")
 
   with open(file, "r", encoding="utf-8") as file_open:
     lines = file_open.readlines()
+
+  http = request.urlopen(EMBED_URL + lines[0].rstrip()).read().decode("utf-8")
+
