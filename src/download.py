d475c5496abf8c903f0726b69c8795a6da144bb2
sevenwhiteclouds <kemaldonado@csumb.edu>
2023-09-14 02:23:41 -0700

    able to download video now

--- src/download.py
+++ src/download.py
@@ -3,12 +3,13 @@ import urllib.request as request
 EMBED_URL = "http://fast.wistia.net/embed/iframe/"
 
 if __name__ == "__main__":
+  print(
   file = input("Enter file name: ")
 
   with open(file, "r", encoding="utf-8") as file_open:
     lines = file_open.readlines()
 
-  # TODO: this should be a try block
+  # TODO: this should probably really be a try block
   # the video is found at line 63 in the html that is returned
   html = request.urlopen(EMBED_URL + lines[0].rstrip()).read().decode("utf-8").split("\n")[63]
 
@@ -17,4 +18,7 @@ if __name__ == "__main__":
 
   video_link = html[link_start : link_end] + ".mp4"
 
-  print(video_link)
+  video = request.urlopen(video_link)
+
+  with open((lines[0].rstrip() + ".mp4"), "wb") as save_video:
+    save_video.write(video.read())
