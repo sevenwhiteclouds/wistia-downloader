dbc9e57c0071f5e6d8a2ab0b71bb37376b533e64
sevenwhiteclouds <kemaldonado@csumb.edu>
2023-09-14 13:13:24 -0700

    comment clarity

--- src/download.py
+++ src/download.py
@@ -17,12 +17,13 @@ if __name__ == "__main__":
 
   for line in lines:
     # TODO: really need to handle cases where video is not able to download. try blocks, maybe.
-    # the video is found at line 63 in the html that is returned
     line = line.rstrip()
     print(f"Downloading video ID [{video_num}/{len(lines)}]: {line}... ", end="", flush=True)
 
+    # the video is found at line 63 in the html that is returned as defined in global const
     html = request.urlopen(EMBED_URL + line).read().decode("utf-8").split("\n")[LINE_NUM]
 
+    # http:// begins after finding "url" and then skipping 6 chars defined in global const
     link_start = html.find("url") + CHARS_TO_H
     link_end = html.find(".bin", link_start) 
 
