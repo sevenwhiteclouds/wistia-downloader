09d99e0683c31f9bb05509c955228a454b43b964
sevenwhiteclouds <kemaldonado@csumb.edu>
2023-09-14 21:57:40 -0700

    async done

--- src/download.py
+++ src/download.py
@@ -3,34 +3,38 @@ import asyncio
 
 CHARS_TO_H = 6
 LINE_NUM = 63
-EVENT_LOOP = asyncio.get_event_loop()
 EMBED_URL = "http://fast.wistia.net/embed/iframe/"
 
+# TODO: really need to handle cases where video is not able to download. try blocks, maybe.
 # this expects the video id to download
-## TODO: really need to handle cases where video is not able to download. try blocks, maybe.
 async def downloader(vid_id):
+  event_loop = asyncio.get_event_loop()
+
   vid_id = vid_id.rstrip()
-  write_file = await EVENT_LOOP.run_in_executor(None, open, f"{vid_id}.mp4", "wb")
+  file_write = await event_loop.run_in_executor(None, open, f"{vid_id}.mp4", "wb")
 
   # the video is found at line 63 in the html that is returned. 63 is defined in global const
-  html = await EVENT_LOOP.run_in_executor(None, req.urlopen, EMBED_URL + vid_id)
-  html = await EVENT_LOOP.run_in_executor(None, html.read)
-  html.close()
+  html = await event_loop.run_in_executor(None, req.urlopen, EMBED_URL + vid_id)
+  html = await event_loop.run_in_executor(None, html.read)
   html = html.decode().split("\n")[LINE_NUM]
 
-  # # http:// begins after finding "url" and then skipping 6 chars. 6 is defined in global const
+  # http:// begins after finding "url" and then skipping 6 chars. 6 is defined in global const
   link_start = html.find("url") + CHARS_TO_H
   link_end = html.find(".bin", link_start) 
 
   video_link = html[link_start : link_end] + ".mp4"
-  video = await EVENT_LOOP.run_in_executor(None, req.urlopen, video_link)
+  video = await event_loop.run_in_executor(None, req.urlopen, video_link)
 
   while True:
-    chunk = await EVEN_LOOP.run_in_executor(None, video.read, 1_000_000)
+    chunk = await event_loop.run_in_executor(None, video.read, 1_000_000)
     if not chunk:
       break
 
-    await EVENT_LOOP.run_in_executor(write_file.write(chunk))
+    await event_loop.run_in_executor(None, file_write.write, chunk)
+
+
+  print("Video downloaded!")
+  await event_loop.run_in_executor(None, file_write.close)
 
 # if running as a module, this expects a list of video ids to be passed in
 async def main(ids):
