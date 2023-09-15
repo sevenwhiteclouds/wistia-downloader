0f3717391d13752c13ef109ac34d26c0e8caeeaa
sevenwhiteclouds <kemaldonado@csumb.edu>
2023-09-14 21:40:58 -0700

    the logic for concurrent is there. need to test

--- src/download.py
+++ src/download.py
@@ -1,18 +1,36 @@
 import urllib.request as req
 import asyncio
 
-EMBED_URL = "http://fast.wistia.net/embed/iframe/"
-LINE_NUM = 63
 CHARS_TO_H = 6
-EVEN_LOOP = asyncio.get_event_loop()
+LINE_NUM = 63
+EVENT_LOOP = asyncio.get_event_loop()
+EMBED_URL = "http://fast.wistia.net/embed/iframe/"
 
 # this expects the video id to download
+## TODO: really need to handle cases where video is not able to download. try blocks, maybe.
 async def downloader(vid_id):
-  write_file = await EVENT_LOOP.run_in_executor(None, open, f"{vid_d}.mp4", "wb")
   vid_id = vid_id.rstrip()
+  write_file = await EVENT_LOOP.run_in_executor(None, open, f"{vid_id}.mp4", "wb")
 
+  # the video is found at line 63 in the html that is returned. 63 is defined in global const
+  html = await EVENT_LOOP.run_in_executor(None, req.urlopen, EMBED_URL + vid_id)
+  html = await EVENT_LOOP.run_in_executor(None, html.read)
+  html.close()
+  html = html.decode().split("\n")[LINE_NUM]
 
-  print("Hello, World!")
+  # # http:// begins after finding "url" and then skipping 6 chars. 6 is defined in global const
+  link_start = html.find("url") + CHARS_TO_H
+  link_end = html.find(".bin", link_start) 
+
+  video_link = html[link_start : link_end] + ".mp4"
+  video = await EVENT_LOOP.run_in_executor(None, req.urlopen, video_link)
+
+  while True:
+    chunk = await EVEN_LOOP.run_in_executor(None, video.read, 1_000_000)
+    if not chunk:
+      break
+
+    await EVENT_LOOP.run_in_executor(write_file.write(chunk))
 
 # if running as a module, this expects a list of video ids to be passed in
 async def main(ids):
@@ -31,26 +49,3 @@ if __name__ == "__main__":
     ids = file_open.readlines()
 
   asyncio.run(main(ids))
-
-  # keeps track of the video downloaded out of the total videos
-  #video_num = 1
-
-  ## TODO: really need to handle cases where video is not able to download. try blocks, maybe.
-  #for id in ids:
-  #  print(f"Downloading video ID [{video_num}/{len(ids)}]: {id}... ", end="", flush=True)
-
-  #  # the video is found at line 63 in the html that is returned. 63 is defined in global const
-  #  html = request.urlopen(EMBED_URL + id).read().decode("utf-8").split("\n")[LINE_NUM]
-
-  #  # http:// begins after finding "url" and then skipping 6 chars. 6 is defined in global const
-  #  link_start = html.find("url") + CHARS_TO_H
-  #  link_end = html.find(".bin", link_start) 
-
-  #  video_link = html[link_start : link_end] + ".mp4"
-  #  video = request.urlopen(video_link)
-
-  #  with open((id + ".mp4"), "wb") as save_video:
-  #    save_video.write(video.read())
-
-  #  print("done!")
-  #  video_num += 1
