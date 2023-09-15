53821ddd94be012efb08212e9e79e0da5bb62713
sevenwhiteclouds <kemaldonado@csumb.edu>
2023-09-14 21:23:41 -0700

    building downloader

--- src/download.py
+++ src/download.py
@@ -1,18 +1,24 @@
-import urllib.request as request
+import urllib.request as req
 import asyncio
 
 EMBED_URL = "http://fast.wistia.net/embed/iframe/"
 LINE_NUM = 63
 CHARS_TO_H = 6
+EVEN_LOOP = asyncio.get_event_loop()
 
-async downloader():
+# this expects the video id to download
+async def downloader(vid_id):
+  write_file = await EVENT_LOOP.run_in_executor(None, open, f"{vid_d}.mp4", "wb")
+  vid_id = vid_id.rstrip()
+
+
   print("Hello, World!")
 
-# if running as a module, this expects a list to be passed in
+# if running as a module, this expects a list of video ids to be passed in
 async def main(ids):
   async with asyncio.TaskGroup() as tasks:
     for vid_id in ids:
-      tasks.create_task(downloader())
+      tasks.create_task(downloader(vid_id))
 
   print("All videos have been downloaded!")
 
@@ -31,7 +37,6 @@ if __name__ == "__main__":
 
   ## TODO: really need to handle cases where video is not able to download. try blocks, maybe.
   #for id in ids:
-  #  id = id.rstrip()
   #  print(f"Downloading video ID [{video_num}/{len(ids)}]: {id}... ", end="", flush=True)
 
   #  # the video is found at line 63 in the html that is returned. 63 is defined in global const
